2b26cf2b07b7d1afa2a28cfc09951fe227832fff
rianrietveld <rian@level-level.com>
2019-12-28 16:19:48 +0100

    Styling background

--- kennel.php
+++ kennel.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<article>
+			<article class="hentry">
 
 				<header class="entry-header">
 					<?php
